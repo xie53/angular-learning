06dc4586c7f9f5b814a41d6498259577ef592694
xie53 <xieys@cn.ibm.com>
2020-04-17 17:28:50 +0800

    create checkout func

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
   items = [];
 
   addToCart(product) {
@@ -19,5 +22,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }
